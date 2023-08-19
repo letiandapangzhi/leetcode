cf2e2470b6d9e3816078b7df79ffc4bbbef7a1a2
大胖纸 <rune@dapangzhideMacBook-Pro.local>
2023-08-19 15:17:17 +0800

    first commit

---
+++ simpler/array/1.go
@@ -0,0 +1,14 @@
+package array
+
+// https://leetcode.cn/problems/two-sum/
+// 1. 两数之和
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
+}
